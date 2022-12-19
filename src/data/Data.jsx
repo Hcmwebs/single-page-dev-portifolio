c32f89568c7a034c1ed463bd235999430a1f3605
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-19 14:17:25 +1300

    refactored the project data

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,12 +1,23 @@
 import project1Large from '../assets/thumbnail-project-1-large.webp'
+import project1Small from '../assets/thumbnail-project-1-small.webp'
+import project2Large from '../assets/thumbnail-project-2-large.webp'
+import project2Small from '../assets/thumbnail-project-2-small.webp'
+import project3Large from '../assets/thumbnail-project-3-large.webp'
+import project3Small from '../assets/thumbnail-project-3-small.webp'
+import project4Large from '../assets/thumbnail-project-4-large.webp'
+import project4Small from '../assets/thumbnail-project-4-small.webp'
+import project5Large from '../assets/thumbnail-project-5-large.webp'
+import project5Small from '../assets/thumbnail-project-5-small.webp'
+import project6Large from '../assets/thumbnail-project-6-large.webp'
+import project6Small from '../assets/thumbnail-project-6-small.webp'
 
 export const skills = [
-	{ id: 1, name: 'HTML', experience: '4 Years Experience' },
-	{ id: 2, name: 'CSS', experience: '4 Years Experience' },
-	{ id: 3, name: 'Javascript', experience: '4 Years Experience' },
-	{ id: 4, name: 'Accessibility', experience: '4 Years Experience' },
-	{ id: 5, name: 'React', experience: '4 Years Experience' },
-	{ id: 6, name: 'SASS', experience: '4 Years Experience' },
+	{ id: 1, title: 'HTML', experience: '4 Years Experience' },
+	{ id: 2, title: 'CSS', experience: '4 Years Experience' },
+	{ id: 3, title: 'Javascript', experience: '4 Years Experience' },
+	{ id: 4, title: 'Accessibility', experience: '4 Years Experience' },
+	{ id: 5, title: 'React', experience: '4 Years Experience' },
+	{ id: 6, title: 'SASS', experience: '4 Years Experience' },
 ]
 
 export const projects = [
@@ -14,54 +25,54 @@ export const projects = [
 		id: 1,
 		title: 'Design portfolio',
 		imageLarge: `${project1Large}`,
-		imageSmall: '../assets/thumbnail-project-1-small.webp',
+		imageSmall: `${project1Small}`,
 		skills: 'HTML CSS',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 	{
 		id: 2,
 		title: 'E-learning landing page',
-		imageLarge: '../assets/thumbnail-project-2-large.webp',
-		imageSmall: '../assets/thumbnail-project-2-small.webp',
+		imageLarge: `${project2Large}`,
+		imageSmall: `${project2Small}`,
 		skills: 'HTML CSS',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 	{
 		id: 3,
 		title: 'Todo web app',
-		imageLarge: '../assets/thumbnail-project-3-large.webp',
-		imageSmall: '../assets/thumbnail-project-3-small.webp',
+		imageLarge: `${project3Large}`,
+		imageSmall: `${project3Small}`,
 		skills: 'HTML CSS Javascript',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 	{
 		id: 4,
 		title: 'Entertainment web app',
-		imageLarge: '../assets/thumbnail-project-4-large.webp',
-		imageSmall: '../assets/thumbnail-project-4-small.webp',
+		imageLarge: `${project4Large}`,
+		imageSmall: `${project4Small}`,
 		skills: 'HTML CSS Javascript',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 	{
 		id: 5,
 		title: 'Memory Game',
-		imageLarge: '../assets/thumbnail-project-5-large.webp',
-		imageSmall: '../assets/thumbnail-project-5-small.webp',
+		imageLarge: `${project5Large}`,
+		imageSmall: `${project5Small}`,
 		skills: 'HTML CSS Javascript',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 	{
 		id: 6,
 		title: 'Art gallery showcase',
-		imageLarge: '../assets/thumbnail-project-6-large.webp',
-		imageSmall: '../assets/thumbnail-project-6-small.webp',
+		imageLarge: `${project6Large}`,
+		imageSmall: `${project6Small}`,
 		skills: 'HTML CSS Javascript',
-		link1: 'view project',
-		link2: 'view code',
+		live: 'view project',
+		code: 'view code',
 	},
 ]
