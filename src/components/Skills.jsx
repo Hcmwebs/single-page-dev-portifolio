d94261ddf7b8eaf7fd61afcd165887d7cac48fcd
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-20 23:17:46 +1300

    Added the code to the skill component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { skills } from '../data/Data'
+import {SingleSkill} from './index'
 
 const Skills = () => {
-  return (
-    <div>Skills component goes here</div>
-  )
+	return (
+		<div>
+			{skills.map((skill) => {
+				return <SingleSkill key={skill.id} {...skill} />
+			})}
+		</div>
+	)
 }
 
 export default Skills
