f2496f543529373b7672db4c16d457374d73ef90
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-22 04:44:03 +1300

    corrected a typo

--- src/styles/index.jsx
+++ src/styles/index.jsx
@@ -2,6 +2,6 @@ import Container from './Container.styled'
 import GlobalStyles from './GlobalStyles'
 import theme from './Themes.styled'
 import StyledHeader from './Header.styled'
-import StyledNav from './NavStyled'
+import StyledNav from './Nav.Styled'
 
 export { GlobalStyles, Container, theme, StyledHeader, StyledNav }
