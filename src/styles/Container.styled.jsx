7ac035d92482977c3169c7ae8aae9a5f2a896f9c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-21 20:49:54 +1300

    added some padding , centered the container and it children

--- src/styles/Container.styled.jsx
+++ src/styles/Container.styled.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-
 	color: ${({ theme }) => theme.darkTheme.body};
 	background-color: ${({ theme }) => theme.darkTheme.bg};
-	width:min(100%, var(--max-width));
-	
+	width: min(100%, var(--max-width));
+	padding-inline: 1rem;
+	margin: 0 auto;
+	display: grid;
+	place-items: center;
 `
 export default Container
