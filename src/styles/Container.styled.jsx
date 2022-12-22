621769ee412e73acbc60eb3e615483e585b0c90f
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-23 09:08:07 +1300

    added media, removed padded and changed the width

--- src/styles/Container.styled.jsx
+++ src/styles/Container.styled.jsx
@@ -3,10 +3,12 @@ import styled from 'styled-components'
 const Container = styled.div`
 	color: ${({ theme }) => theme.darkTheme.body};
 	background-color: ${({ theme }) => theme.darkTheme.bg};
-	width: min(100%, var(--max-width));
-	padding-inline: 1rem;
+	width: min(90vw, var(--max-width));
 	margin: 0 auto;
 	display: grid;
 	place-items: center;
+	@media (min-width: ${({ theme }) => theme.screens.tablet}) {
+		width: 95vw;
+	}
 `
 export default Container
