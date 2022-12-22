6a23056932b0af28694d6eeea9b92d49574e663c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-23 09:26:21 +1300

    fixed the inline padding and the width

--- src/styles/Container.styled.jsx
+++ src/styles/Container.styled.jsx
@@ -3,12 +3,16 @@ import styled from 'styled-components'
 const Container = styled.div`
 	color: ${({ theme }) => theme.darkTheme.body};
 	background-color: ${({ theme }) => theme.darkTheme.bg};
-	width: min(90vw, var(--max-width));
+	width: min(100%, var(--maxWidth));
+	padding-inline: 1rem;
 	margin: 0 auto;
 	display: grid;
 	place-items: center;
 	@media (min-width: ${({ theme }) => theme.screens.tablet}) {
-		width: 95vw;
+		padding-inline: 2rem;
+	}
+	@media (min-width: ${({ theme }) => theme.screens.desktop}) {
+		padding-inline: 10rem;
 	}
 `
 export default Container
