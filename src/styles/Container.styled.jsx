d078db53c185f937e4815ed5fa7320240f1b39b9
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-24 09:22:21 +1300

    added pseudos before and after the main container
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Container.styled.jsx
+++ src/styles/Container.styled.jsx
@@ -4,15 +4,38 @@ const Container = styled.div`
 	color: ${({ theme }) => theme.darkTheme.body};
 	background-color: ${({ theme }) => theme.darkTheme.bg};
 	width: min(100%, var(--maxWidth));
+	height: 100%;
 	padding-inline: 1rem;
 	margin: 0 auto;
 	display: grid;
 	place-items: center;
+	position: relative;
 	@media (min-width: ${({ theme }) => theme.screens.tablet}) {
 		padding-inline: 2rem;
 	}
 	@media (min-width: ${({ theme }) => theme.screens.desktop}) {
 		padding-inline: 10rem;
+	}
+	&::before,
+	&::after {
+		content: '';
+		position: absolute;
+		width: 100px;
+		height: 100px;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	&::before {
+		background-position: center center;
+		background-color: #fff;
+		background-image: url('/pattern-rings.svg');
+		z-index: 1;
+	}
+	&::after {
+		background-position: center center;
+		background-color: var(--accent);
+		background-image: url('/pattern-circle.svg');
+		z-index: 2;
 	}
 `
 export default Container
