47fbdc677b706f7a62bf8a5245199a8e28b8de97
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-25 08:00:24 +1300

    added a hero header
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,13 @@ import profileLarge from '../assets/image-profile-desktop.webp'
 const Hero = () => {
 	return (
 		<div>
-			<picture>
-				<source srcset={profileLarge} media='(min-width: 1440px)' />
-				<source srcset={profileMedium} media='(min-width: 768px)' />
-				<img src={profileSmall} alt='profile' />
-			</picture>
+			<div className='heroHeader'>
+				<picture>
+					<source srcset={profileLarge} media='(min-width: 1440px)' />
+					<source srcset={profileMedium} media='(min-width: 768px)' />
+					<img src={profileSmall} alt='profile' />
+				</picture>
+			</div>
 			<div className='hero-body'>
 				<h1>
 					Nice to meet you! I'm <span>Adam Keyes.</span>
