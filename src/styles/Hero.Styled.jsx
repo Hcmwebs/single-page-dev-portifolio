22dfa046a4ba7a2bf67f330bbfb7279ca0e57575
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-25 09:23:34 +1300

    started styling the Hero
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Hero.Styled.jsx
+++ src/styles/Hero.Styled.jsx
@@ -1,6 +1,27 @@
-import styled  from 'styled-components'
+import styled from 'styled-components'
 
 const StyledHero = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	gap: 2.5rem;
+	.hero-header {
+		width: 190px;
+		height: 242px;
+		display: grid;
+		place-items: center;
+
+		img {
+			width: 100%;
+			height: 100%;
+      margin-top: -9rem
+		}
+	}
+  .hero-body{
+
+  }
 `
 
 export default StyledHero
