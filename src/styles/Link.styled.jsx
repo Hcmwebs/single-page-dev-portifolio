5e068b3d8004097d1b50ceb0cb0945030e039bc1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-26 09:04:46 +1300

    created and indexed styled link

---
+++ src/styles/Link.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const StyledLink = styled.a`
+	text-decoration: none;
+	width: 117px;
+	height: 26px;
+	font-weight: 700;
+	font-size: 1rem;
+	line-height: 26px;
+	letter-spacing: 2.28571px;
+	position: relative;
+
+	&::before {
+	}
+`
+
+export default StyledLink
