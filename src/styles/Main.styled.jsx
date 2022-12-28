ed0b921e3ae53358cfe5a62d2f1a4692d42a145e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-28 21:41:51 +1300

    added gap and padding block
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Main.styled.jsx
+++ src/styles/Main.styled.jsx
@@ -1,11 +1,18 @@
-import styled from 'styled-components';
-
+import styled from 'styled-components'
 
 const StyledMain = styled.main`
-
-width: 100%;
-display: grid;
-place-items: center;
-
+	width: 100%;
+	padding-block: 5rem;
+	display: grid;
+	place-items: center;
+	gap: 5rem;
+	@media (min-width: 768px) {
+		padding-block: 4rem;
+		gap: 6.25rem;
+	}
+	@media (min-width: 1440px) {
+		padding-block: 6.5rem;
+		gap: 8.75rem;
+	}
 `
 export default StyledMain
