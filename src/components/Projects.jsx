8392513aa7db43ad3ac48598e0aeeafcfd8d39e8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-30 01:11:30 +1300

    added more content to the project component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { projects } from '../data/Data'
 import { SingleProject } from './index'
-import { StyledSection } from '../styles'
+import { StyledTitle, StyledLink, StyledSection } from '../styles'
 
 const Projects = () => {
 	return (
 		<StyledSection>
+			<div>
+				<StyledTitle>
+					<h2>Projects</h2>
+				</StyledTitle>
+				<StyledLink href='#' target='_blank' rel='noopener noreferrer'>
+					contact me
+				</StyledLink>
+			</div>
 			<div className='items'>
 				{projects.map((project) => {
 					return <SingleProject key={project.id} {...project} />
