510c3920bf760c9834e79a7b2ece04f1ed5a3f9c
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-30 03:01:04 +1300

    refactored the code
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Singleproject.jsx
+++ src/components/Singleproject.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const SingleProject = ({id,title,imageLarge,live,code,skills}) => {
+const SingleProject = ({
+	id,
+	title,
+	imageLarge,
+	imageSmall,
+	live,
+	code,
+	skills,
+}) => {
 	return (
 		<div className='project' key={id}>
-			<img src={imageLarge} alt={title} />
+			<picture>
+				<source
+					srcSet={imageLarge}
+					media='(min-width: 768px)'
+					type='image/webp'
+				/>
+				<source
+					srcSet={imageSmall}
+					media='(min-width: 300px) and (max-width:767px) '
+					type='image/webp'
+				/>
+				<img src={imageSmall} alt={title} type='image/webp' />
+			</picture>
+
 			<div className='cardBody'>
 				<h2>{title}</h2>
 				<p className='skills'>{skills}</p>
