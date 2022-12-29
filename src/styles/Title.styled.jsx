da679ff610794cea78c968fe3c87a2f710e895f2
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-30 01:45:45 +1300

    used font weight variable
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Title.styled.jsx
+++ src/styles/Title.styled.jsx
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 const StyledTitle = styled.div`
 	h2 {
 		width: max-content;
-		font-family: 'Space Grotesk';
-		font-weight: ${({ theme }) => theme.fontWeights.bold};
 		font-size: 40px;
 		line-height: 40px;
 		text-align: center;
