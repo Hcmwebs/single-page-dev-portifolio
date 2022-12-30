e16589f3458678b23216e123135b255d9ca62c06
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-31 08:19:27 +1300

    changed the wrapper into Styledproject
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Singleproject.jsx
+++ src/components/Singleproject.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import { StyledProject } from '../styles'
 
 const SingleProject = ({
 	id,
@@ -11,55 +11,47 @@ const SingleProject = ({
 	skills,
 }) => {
 	return (
-		<Wrapper key={id}>
-			<picture>
-				<source
-					srcSet={imageLarge}
-					media='(min-width: 768px)'
-					type='image/webp'
-				/>
-				<source
-					srcSet={imageSmall}
-					media='(min-width: 300px) and (max-width:767px) '
-					type='image/webp'
-				/>
-				<img src={imageSmall} alt={title} type='image/webp' />
-			</picture>
+		<StyledProject>
+			<div key={id}>
+				<picture>
+					<source
+						srcSet={imageLarge}
+						media='(min-width: 768px)'
+						type='image/webp'
+					/>
+					<source
+						srcSet={imageSmall}
+						media='(min-width: 300px) and (max-width:767px) '
+						type='image/webp'
+					/>
+					<img src={imageSmall} alt={title} type='image/webp' />
+				</picture>
 
-			<div className='cardBody'>
-				<h2>{title}</h2>
-				<p className='skills'>{skills}</p>
-			</div>
-			<div className='cardFooter'>
-				<div className='links'>
-					<a
-						href={live}
-						target='_blank'
-						rel='noopener noreferrer'
-						className='btn'>
-						{live}
-					</a>
-					<a
-						href={code}
-						target='_blank'
-						rel='noopener noreferrer'
-						className='btn'>
-						{code}
-					</a>
+				<div className='cardBody'>
+					<h2>{title}</h2>
+					<p className='skills'>{skills}</p>
+				</div>
+				<div className='cardFooter'>
+					<div className='links'>
+						<a
+							href={live}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='btn'>
+							{live}
+						</a>
+						<a
+							href={code}
+							target='_blank'
+							rel='noopener noreferrer'
+							className='btn'>
+							{code}
+						</a>
+					</div>
 				</div>
 			</div>
-		</Wrapper>
+		</StyledProject>
 	)
 }
-const Wrapper = styled.div`
-width: min(100%, 343px);
-min-height:450px;
-display:grid;
-place-items: center;
-
-@media (min-width: 768px) {
-	width: min(100%, 540px);
-}
-`
 
 export default SingleProject
