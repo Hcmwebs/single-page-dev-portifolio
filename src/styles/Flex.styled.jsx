3e6a363b95ad358507f5fa4fa34398bdd271748b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-31 19:01:03 +1300

    changed the width to be dynamic using props
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Flex.styled.jsx
+++ src/styles/Flex.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const StyledFlex = styled.div`
-	width: 100%;
+	width: ${({ width }) => width};
 	display: flex;
 	justify-content: ${({ jc }) => jc};
 	align-items: ${({ align }) => align};
