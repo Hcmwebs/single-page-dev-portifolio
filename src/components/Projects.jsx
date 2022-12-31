d203781f5355afad71f92ba65434e774bbe28a99
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-31 18:59:24 +1300

    added a width prop to the flex componet
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import { StyledTitle, StyledLink, StyledSection, StyledFlex } from '../styles'
 const Projects = () => {
 	return (
 		<StyledSection>
-			<StyledFlex jc='space-between' align='center'>
+			<StyledFlex jc='space-between' align='center' width='100%'>
 				<StyledTitle>
 					<h2>Projects</h2>
 				</StyledTitle>
