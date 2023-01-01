ed067d01c1f3806196fe88066e8e89ed08f572c0
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-01 23:07:24 +1300

    moved the padding in-line into header and mainBody
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Header.styled.jsx
+++ src/styles/Header.styled.jsx
@@ -4,6 +4,13 @@ const StyledHeader = styled.header`
 	width: 100%;
 	display: grid;
 	place-items: center;
+	padding-inline: 1rem;
+	@media (min-width: ${({ theme }) => theme.screens.tablet}) {
+		padding-inline: 2rem;
+	}
+	@media (min-width: ${({ theme }) => theme.screens.desktop}) {
+		padding-inline: 10rem;
+	}
 `
 
 export default StyledHeader
--- src/styles/Main.styled.jsx
+++ src/styles/Main.styled.jsx
@@ -2,16 +2,16 @@ import styled from 'styled-components'
 
 const StyledMain = styled.main`
 	width: 100%;
-	padding-block: 5rem;
+	padding: 5rem 1rem;
 	display: grid;
 	place-items: center;
 	gap: 5rem;
 	@media (min-width: 768px) {
-		padding-block: 4rem;
+		padding: 4rem 2rem;
 		gap: 6.25rem;
 	}
 	@media (min-width: 1440px) {
-		padding-block: 6.5rem;
+		padding: 6.5rem 10rem;
 		gap: 8.75rem;
 	}
 `
