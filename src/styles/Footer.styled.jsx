ca1f46353b09e9999fe60345514701b21a5394be
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-04 07:39:05 +1300

    Removed the gap and the hr's inline padding
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Footer.styled.jsx
+++ src/styles/Footer.styled.jsx
@@ -4,19 +4,11 @@ const StyledFooter = styled.footer`
 	width: 100%;
 	display: grid;
 	place-items: center;
-	gap: 2.5rem;
 	padding-block: 3.75rem;
 	background-color: ${({ theme }) => theme.darkTheme.footer};
 	margin-top: auto;
 	hr {
 		width: 100%;
-		padding-inline: 1rem;
-		@media (min-width: ${({ theme }) => theme.screens.tablet}) {
-			padding-inline: 2rem;
-		}
-		@media (min-width: ${({ theme }) => theme.screens.desktop}) {
-			padding-inline: 10rem;
-		}
 	}
 `
 
