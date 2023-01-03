e5fc779211da34959fcf4b6ea625e8ba201e6fa7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-04 07:35:23 +1300

    added container to the footer
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Form } from './index'
-import { StyledFooter } from '../styles'
+import { Container, StyledFooter } from '../styles'
 
 const Footer = () => {
 	return (
 		<StyledFooter mainFooter>
-			<Form />
-			<hr />
-			<div>Footer nav goes here</div>
+			<Container>
+				<Form />
+				<hr />
+				<div>Footer nav goes here</div>
+			</Container>
 		</StyledFooter>
 	)
 }
