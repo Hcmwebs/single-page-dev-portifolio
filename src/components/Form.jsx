53dbd4de65967b67ea26485be4870a58928d9edb
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-04 08:02:41 +1300

    started styling the contact section
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Form = () => {
-  return (
-    <div>Form component goes here</div>
-  )
+	return (
+		<div>
+			<div className='intro'>
+				<h2>Contact</h2>
+				<p>
+					I would love to hear about your project and how I could help. Please
+					fill in the form, and I’ll get back to you as soon as possible.
+				</p>
+			</div>
+			<form>
+				<div className='form-group'>
+					<input type='text' name='Name' id='name' />
+				</div>
+        <button type='submit'>Send Message</button>
+			</form>
+		</div>
+	)
 }
 
 export default Form
