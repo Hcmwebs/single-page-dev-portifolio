c66fe0bd6f3872a731b30e6f5efd82446659032e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-08 01:14:06 +1300

    removed the logo and socials components
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/styles/Nav.Styled.jsx
+++ src/styles/Nav.Styled.jsx
@@ -15,23 +15,7 @@ const StyledNav = styled.nav`
 		width: 100%;
 		flex-direction: row;
 	}
-	.logo {
-		font-size: 1.5rem;
-		font-weight: ${({ theme }) => theme.fontWeights.bold};
-		line-height: 2rem;
-		color: ${({ theme }) => theme.darkTheme.header};
-		text-align: center;
-		@media (min-width: ${({ theme }) => theme.screens.tablet}) {
-			font-size: 2rem;
-		}
-	}
 
-	.socials {
-		display: grid;
-		place-items: center;
-		grid-template-columns: repeat(4, 1fr);
-		gap: 1.625rem;
-	}
 	a {
 		&:hover .icon > path {
 			transition: var(--transition-2);
