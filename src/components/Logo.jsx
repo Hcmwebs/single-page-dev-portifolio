dd3933f46174fa5ec0d74946f9265f65ef3fe60e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-08 01:05:01 +1300

    Styled the Logo component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { StyledLogo } from '../styles'
 
 const Logo = () => {
-  return (
-    <div>Logo here</div>
-  )
+	return (
+		<>
+			<StyledLogo
+				href='https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub'
+				target='_blank'
+				rel='noopener noreferrer'>
+				adamkeyes
+			</StyledLogo>
+		</>
+	)
 }
 
 export default Logo
