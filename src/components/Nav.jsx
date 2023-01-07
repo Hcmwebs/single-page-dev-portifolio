80262c09a2d3500be0f8c361686a93ad7d7b5cfc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-08 01:05:37 +1300

    Used the Logo Component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { StyledNav } from '../styles'
-import { Socials } from '../components'
+import { Logo, Socials } from '../components'
 
 const Nav = () => {
 	return (
 		<StyledNav>
-			<a
-				href='https://www.frontendmentor.io/challenges/singlepage-developer-portfolio-bBVj2ZPi-x/hub'
-				target='_blank'
-				rel='noopener noreferrer'
-				className='logo'>
-				adamkeyes
-			</a>
+			<Logo />
 			<Socials />
 		</StyledNav>
 	)
